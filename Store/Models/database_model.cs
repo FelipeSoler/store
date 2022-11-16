b5fe60030ff4308f5e3bbd27e112827cb5d33e01
FelipeSoler <jsolerrosas@gmail.com>
2022-11-15 22:12:41 -0500

    Mi primer commit

---
+++ Store/Models/database_model.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlTypes;
+
+namespace Store.Models
+{
+    public class database_model
+    {
+        private int id { 
+            get { return id; }
+            set { id = value; }
+        }
+        private string sku {
+            get { return sku; }
+            set { sku = value; }  }
+        string product_name {
+            get { return product_name; } 
+            set { product_name = value; } 
+        }
+        SqlMoney price {
+            get { return price; } 
+            set { price = value; } 
+        }
+
+    }
+}
